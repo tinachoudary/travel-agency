2e279fe0b2ebbb22b8edd705dc5e9aff13cdb363
tinachoudary <82216875+tinachoudary@users.noreply.github.com>
2023-12-07 17:20:34 +0530

    Update functions.php

--- functions/functions.php
+++ functions/functions.php
@@ -1,6 +1,6 @@
 <?php
 
-$con = mysqli_connect("localhost", "root", "", "tagency");
+$con = mysqli_connect("tagency.mysql.database.azure.com", "tina", "Banti*7176", "tagency");
 
 if (mysqli_connect_errno()) {
     echo "Failed to connect to MySQL: " . mysqli_connect_error();
