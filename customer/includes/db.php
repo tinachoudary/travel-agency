ff560cecb6fca858f5615a5b3f39011012ff1e87
tinachoudary <82216875+tinachoudary@users.noreply.github.com>
2023-12-07 15:19:30 +0530

    Update db.php

--- customer/includes/db.php
+++ customer/includes/db.php
@@ -1,5 +1,5 @@
 <?php
-$con = mysqli_connect("tagency.mysql.database.azure.com", "tina", "", "tagency");
+$con = mysqli_connect("tagency.mysql.database.azure.com", "tina", "Banti*7176", "tagency");
 if (mysqli_connect_errno()) {
     echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
